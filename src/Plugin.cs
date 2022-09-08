5dba1012d662972a950a0ddd455b6f410513f61d
Dual <31146412+Dual-Iron@users.noreply.github.com>
2022-09-08 14:55:08 -0400

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace RainWorldPlugin
+{
+    [BepInPlugin("org.author.rainworldplugin", nameof(RainWorldPlugin), "0.1.0")]
+    public sealed class Plugin : BaseUnityPlugin
+    {
+        public static new ManualLogSource Logger { get; private set; }
+
+        public void OnEnable()
+        {
+            Logger = base.Logger;
+
+            On.RainWorld.Start += RainWorld_Start;
+        }
+
+        private void RainWorld_Start(On.RainWorld.orig_Start orig, RainWorld self)
+        {
+            orig(self);
+
+            Logger.LogInfo("Hello, world!");
+        }
+    }
+}
