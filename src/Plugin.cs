d23fc87048f3e0d8cd1cdd05d4068b84a0cf9754
Dual-Iron <31146412+Dual-Iron@users.noreply.github.com>
2022-09-09 11:52:25 -0400

    Check `IsMe`

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -8,12 +8,14 @@ namespace LavaCat
     sealed class Plugin : BaseUnityPlugin
     {
         public static new ManualLogSource Logger { get; private set; }
+        public static LavaCatCharacter Char { get; private set; }
 
         public void OnEnable()
         {
             Logger = base.Logger;
+            Char = new LavaCatCharacter();
 
-            PlayerManager.RegisterCharacter(new LavaCatCharacter());
+            PlayerManager.RegisterCharacter(Char);
 
             On.Player.Update += Player_Update;
         }
@@ -21,6 +23,11 @@ namespace LavaCat
         private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
         {
             orig(self, eu);
+
+            // If not lavacat, do nothing.
+            if (!Char.IsMe(self)) {
+                return;
+            }
 
             // Emit some fire particles from the center of the head, just to verify that the mod is working
             if (self.room != null && UnityEngine.Random.value < 0.2f) {
